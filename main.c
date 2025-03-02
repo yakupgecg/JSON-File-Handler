f5400f6d0af65af3471b495955da883414b13186
Yakup <yakup.jarvenpaa@hotmail.com>
2025-03-02 19:10:55 +0200

    Add reset functions, freepair(); Update mapToJson: auto buffersize

--- main.c
+++ main.c
@@ -28,89 +28,113 @@ map_t *initalizemap() {
     return var1;
 }
 
-// This will replace the hashmaps key and value and valuetype with a new one
-int resetpair(map_t* hm, char *key, void *value, int valuetype) {
-    if (hm == NULL) {
+// Next: Add a resto functions for every datatype like a restoint or restostring function
+
+int resetkey(map_t *pair, char *key) {
+    free(pair->key);
+    pair->key = strdup(key);
+    if (pair->key == NULL) {
         return 1;
     }
-    hm->key = strdup(key);
-    hm->value = value;
-    hm->valuetype = valuetype;
-    hm->next = NULL;
     return 0;
 }
 
-// This will add a new pair to the end of the hashmap
-int addlast(map_t *hm, char *key, void *value, int valuetype) {
-    map_t* current = hm;
-    if (hm == NULL) {
-        return 1;
+int restoint(map_t *pair, int value) {
+    if (pair->value == NULL) {
+        pair->value = malloc(sizeof(int));
+        if (pair->value == NULL) {
+            return 1;
+        }
     }
-    while (current->next != NULL) {
-        current = current->next;
-    }
-    current->next = initalizemap();
-    current->next->key = strdup(key);
-    current->next->value = value;
-    current->next->valuetype = valuetype;
-    current->next->next = NULL;
+    *(int*)pair->value = value;
+    pair->valuetype = INT;
     return 0;
 }
 
-// Adds a pair after another pair in a hashmap
-void addafter(map_t *hm, char *key, void *value, int valuetype) {
-    map_t *hmnext = hm->next;
-    map_t *newmap = initalizemap();
-    newmap->key = strdup(key);
-    newmap->value = value;
-    newmap->valuetype = valuetype;
-    hm->next = newmap;
-    newmap->next = hmnext;
-}
-
-
-// This will delete the last pair of the hashmap
-int deletelast(map_t *hm) {
-    map_t *current = hm;
-    if (current->next == NULL) {
-        free(current);
-        return 0;
-    }
-    while (current->next != NULL) {
-        if (current->next->next == NULL) {
-            free(current->next);
-            current->next = NULL;
-            return 0;
+int restoshort(map_t *pair, short value) {
+    if (pair->value == NULL) {
+        pair->value = malloc(sizeof(short));
+        if (pair->value == NULL) {
+            return 1;
         }
-        current = current->next;
     }
-    return 1;
+    *(short*)pair->value = value;
+    pair->valuetype = SHR;
+    return 0;
 }
 
-// This will get a hashmap out by name. If hm is the base of all the other pairs in that specific hashmap, the next pair of hm is to be classified as the base.
-int deletebykey(map_t *hm, const char *keytofind) {
-    map_t *current = hm;
-    map_t *prev = NULL;
-
-    if (strcmp(current->key, keytofind) == 0) {
-        prev = current;
-        current = current->next;
-        free(prev);
-        return 0;
-    }
-    prev = hm;
-    while (current != NULL) {
-        if (strcmp(current->key, keytofind) == 0) {
-            prev->next = current->next;
-            free(current);
-            return 0;
+int restofloat(map_t *pair, float value) {
+    if (pair->value == NULL) {
+        pair->value = malloc(sizeof(float));
+        if (pair->value == NULL) {
+            return 1;
         }
-        prev = current;
-        current = current->next;
     }
-    return 1;
+    *(float*)pair->value = value;
+    pair->valuetype = FLT;
+    return 0;
 }
 
+int restostring(map_t *pair, char *val_ptr, int str_len) {
+    if (pair->value == NULL) {
+        pair->value = malloc(str_len * sizeof(char));
+        if (pair->value == NULL) {
+            return 1;
+        }
+    }
+    *(char*)pair->value = val_ptr;
+    pair->valuetype = STR;
+    return 0;
+}
+
+int restodouble(map_t *pair, double value) {
+    if (pair->value == NULL) {
+        pair->value = malloc(sizeof(double));
+        if (pair->value == NULL) {
+            return 1;
+        }
+    }
+    *(double*)pair->value = value;
+    pair->valuetype = DBL;
+    return 0;
+}
+
+int restolong(map_t *pair, long value) {
+    if (pair->value == NULL) {
+        pair->value = malloc(sizeof(long));
+        if (pair->value == NULL) {
+            return 1;
+        }
+    }
+    *(long*)pair->value = value;
+    pair->valuetype = LONG;
+    return 0;
+}
+
+int restolonglong(map_t *pair, long long value) {
+    if (pair->value == NULL) {
+        pair->value = malloc(sizeof(long long));
+        if (pair->value == NULL) {
+            return 1;
+        }
+    }
+    *(long long*)pair->value = value;
+    pair->valuetype = LL;
+    return 0;
+}
+
+int restolongdouble(map_t *pair, long double value) {
+    if (pair->value == NULL) {
+        pair->value = malloc(sizeof(long double));
+        if (pair->value == NULL) {
+            return 1;
+        }
+    }
+    *(long double*)pair->value = value;
+    pair->valuetype = LDBL;
+    return 0;
+}
+ 
 // Calculates the length of the hashmap, but if the pair is not the root of the hashmap, it will start from pair
 size_t maplen(map_t *hm) {
     size_t len = 0;
@@ -127,16 +151,67 @@ void freemap(map_t* hm) {
     map_t* next;
     while (current != NULL) {
         next = current->next;
+        if (current->key != NULL) {
+            free(current->key);
+        }
+        if (current->value != NULL) {
+            free(current->value);
+        }
         free(current);
         current = next;
     }
 }
 
+void freepair(map_t *pair) {
+    if (pair->key != NULL) {
+        free(pair->key);
+    }
+    if (pair->value != NULL) {
+        free(pair->value);
+    }
+    free(pair);
+}
+
 // For translating hashmaps to JSON file format
 char *mapToJson(map_t *hm, size_t size) {
-    // Next: Add automatic buffersizer
     map_t *current = hm;
-    char *buffer = malloc(size);
+    int buffersize = 50; // Buffersize is a bit bigger for backup memory just in case
+    while (current != NULL) {
+        buffersize += 2 // For the , and space
+        buffersize += strlen(current->key) + 2;
+        switch (current->valuetype) {
+            case INT:
+                buffersize += 11;
+                break;
+            case FLT:
+                buffersize += 47;
+                break;
+            case DBL:
+                buffersize += 325;
+                break;
+            case LONG:
+                buffersize += 15;
+            case LL:
+                buffersize += 20
+                break;
+            case LDBL:
+                buffersize += 4932;
+                break;
+            case STR:
+                buffersize += strlen(current->value) + 2;
+                break;
+            case SHR:
+                buffersize += 6;
+                break;
+            case RAW:
+                buffersize += strlen(current->value);
+            default:
+                buffersize += 10;
+                break;
+        }
+    }
+    current = hm;
+    char *buffer = malloc(buffersize);
     if (buffer == NULL) {
         return NULL;
     }
@@ -208,7 +283,40 @@ char *mapToJson(map_t *hm, size_t size) {
 }
 
 // For translating a pair into JSON file format
-char *pairToJson(map_t *hm, size_t size) {
+char *pairToJson(map_t *hm) {
+    int buffersize = 20;
+    buffersize += 2 // For the , and space
+    buffersize += strlen(current->key) + 2;
+    switch (hm->valuetype) {
+        case INT:
+            buffersize += 11;
+            break;
+        case FLT:
+            buffersize += 47;
+            break;
+        case DBL:
+            buffersize += 325;
+            break;
+        case LONG:
+            buffersize += 15;
+        case LL:
+            buffersize += 20
+            break;
+        case LDBL:
+            buffersize += 4932;
+            break;
+        case STR:
+            buffersize += strlen(current->value) + 2;
+            break;
+        case SHR:
+            buffersize += 6;
+            break;
+        case RAW:
+            buffersize += strlen(current->value);
+        default:
+            buffersize += 10;
+            break;
+    }
     char *json = malloc(size);
     if (json == NULL) {
         return NULL;
