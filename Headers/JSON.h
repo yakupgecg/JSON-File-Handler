92b6d795d9df0c1fc3b1caeff450c5ca2495fde8
Yakup <yakup.jarvenpaa@hotmail.com>
2025-03-12 17:18:03 +0200

    Add new multiline comment to JSON.h to tell about features coming soon

--- Headers/JSON.h
+++ Headers/JSON.h
@@ -7,4 +7,24 @@
 #include "json-handler.h"
 #include "functions.h"
 
+/*
+
+Hello. Because there has not been many updates lately, here is a list of all the features coming soon
+    -Lists like {"Example": ["foo", "bar", 12, null]}
+    -A function turn numbers and floating point numbers to strings to avoid extra valuetypes
+    -Nested lists like {"Example": ["foo", "bar", [5, false]]}
+    -Nested objects like  {"Example": {"Foo": 12, "Bar": true}}
+    -Syntax checker on JSON strings or files
+    -JSON string or file decoder 
+    -As much customization as possible
+    -Finish the project by fixing bugs and ensuring memory management is safe
+
+At the time I have made all these updates and improvements
+this project will probably be in v1.0.0
+
+I might be adding new and better improvements but still rarely, since
+after v1.0.0 I will work on a new project
+
+*/
+
 #endif
