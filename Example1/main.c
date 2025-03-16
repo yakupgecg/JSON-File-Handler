9e4484ec576d433509bbbf516d19483d143554ee
Yakup <yakup.jarvenpaa@hotmail.com>
2025-03-16 17:44:00 +0200

    Add Examples

---
+++ Example1/main.c
@@ -0,0 +1,21 @@
+#include "JSON.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE *file = fopen("Test.json", "w");
+    map_t *map = initM();
+
+    setrawH(resetkey(map, "Name", 4), "\"CSON\"", 6);
+    setrawH(resetkey(appendH(map), "Version", 7), "\"v0.9.4\"", 8);
+    setrawH(resetkey(appendH(map), "Age_In_Days", 11), "20", 2);
+    setrawH(resetkey(appendH(map), "isFinished", 10), "false", 5);
+
+    char *json = maptoJSON(map);
+    fputs(json, file);
+    free(json);
+    freemap(map);
+    fclose(file);
+    return 0;
+}
---
+++ Example2/main.c
@@ -0,0 +1,52 @@
+#include "JSON.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	FILE *file = fopen("Test.json", "w");
+	map_t *root = initM();
+	map_t *subroot = initM();
+	list_t *source_Files = initL();
+	list_t *header_Files = initL();
+	char *goalstring = "{\"JSON-File-Handler\": {\"Headers\": [\"structure.h\", \"parser.h\", \"json-encoder.h\", \"functions.h\", \"JSON.h\"], \"Source Files\": [\"structure.c\", \"parser.c\", \"json-encoder.c\", \"functions.c\"]}}";
+
+	setrawL(source_Files, "\"structure.c\"", 13);
+	setrawL(appendL(source_Files), "\"parser.c\"", 10);
+	setrawL(appendL(source_Files), "\"json-encoder.c\"", 16);
+	setrawL(appendL(source_Files), "\"functions.c\"", 13);
+
+	setrawL(header_Files, "\"structure.h\"", 13);
+	setrawL(appendL(header_Files), "\"parser.h\"", 10);
+	setrawL(appendL(header_Files), "\"json-encoder.h\"", 16);
+	setrawL(appendL(header_Files), "\"functions.h\"", 13);
+	setrawL(appendL(header_Files), "\"JSON.h\"", 8);
+
+	char *header_Files_json = listoJSON(header_Files);
+	char *source_Files_json = listoJSON(source_Files);
+
+	setrawH(resetkey(subroot, "Headers", 7), header_Files_json, strlen(header_Files_json));
+	setrawH(resetkey(appendH(subroot), "Source Files", 12), source_Files_json, strlen(source_Files_json));
+
+	char *json = maptoJSON(subroot);
+
+	setrawH(resetkey(root, "JSON-File-Handler", 17), json, strlen(json));
+
+	json = maptoJSON(root);
+
+	if (strcmp(json, goalstring) == 0) {
+		printf("Success!");
+		fputs(json, file);
+	} else {
+		printf("Unsuccess!");
+	}
+
+	free(json);
+	free(header_Files_json);
+	free(source_Files_json);
+	freelist(header_Files);
+	freelist(source_Files);
+	freemap(subroot);
+	freemap(root);
+	return 0;
+}
