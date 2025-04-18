ea4eea75eb41b4814db4b01012f3f02c7ec8a748
Yakup <yakup.jarvenpaa@hotmail.com>
2025-04-18 13:50:59 +0300

    Add Example3

---
+++ Examples/Example3/main.c
@@ -0,0 +1,35 @@
+#include "JSON.h"
+#include <stdio.h>
+#include <string.h>
+
+int CtoF(int cel) {
+    return cel * 1.8 + 32;
+}
+
+int main() {
+    FILE *file = fopen("Test.json", "w");
+    int cel = 15;
+    int fahrnt = CtoF(cel);
+    char *goalstring = "{\"Celsius\": 15, \"Fahrenheit\": 59}";
+    obj_t *root = resetkey(initM(), "Celsius", 7);
+
+    setrawH(root, parseInt(cel), strlen(parseInt(cel)));
+    resetkey(appendH(root), "Fahrenheit", 10);
+    setrawH(getpairbykey(root, "Fahrenheit"), parseInt(fahrnt), strlen(parseInt(fahrnt)));
+
+    char *json = maptoJSON(root);
+    
+    if (strcmp(goalstring, json) == 0) {
+        printf("Successful\n");
+        fputs(json, file);
+    } else {
+        printf("Unsuccessful\n\nExpected: %s\nResult: %s\n", goalstring, json);
+        freemap(root);
+        fclose(file);
+        return 1;
+    }
+
+    freemap(root);
+    fclose(file);
+    return 0;
+}
