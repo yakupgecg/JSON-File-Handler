000e174aa570bcee76e8b7e73818114cc1d4379b
Yakup <yakup.jarvenpaa@hotmail.com>
2025-10-16 22:38:41 +0300

    Update the structs' value from void * to own datatype json_value_t

--- Headers/data_structures.h
+++ Headers/data_structures.h
@@ -6,16 +6,16 @@
 /* This header defines structs, valuetypes, macros and variables */
 
 enum valuetype {
-    RAW, // Raw data (string)
-    SHR, // Short
-    INT, // Integer
-    FLT, // Float
-    DBL, // Double
-    LONG, // Long
-    LL, // Long Long
-    LDBL, // Long double
-    LIST, // Nested list
-    NMAP // Nested map
+    RAW,   // Raw data (string)
+    SHR,   // Short
+    INT,   // Integer
+    FLT,   // Float
+    DBL,   // Double
+    LONG,  // Long
+    LL,    // Long Long
+    LDBL,  // Long double
+    LIST,  // Nested list
+    NMAP   // Nested map
 };
 
 #ifndef EJSON
@@ -23,25 +23,42 @@ enum valuetype {
 #endif
 
 // These variables below are for encoding functions to allocate enough memory for each type
-extern unsigned int SHR_STR_LEN; // The maximum length of a string made from a Short
-extern unsigned int INT_STR_LEN; // The maximum length of a string made from an Integer
-extern unsigned int FLT_STR_LEN; // The maximum length of a string made from a Float
-extern unsigned int DBL_STR_LEN; // The maximum length of a string made from a Double
-extern unsigned int LONG_STR_LEN; // The maximum length of a string made from a Long
-extern unsigned int LL_STR_LEN;  // The maximum length of a string made from a Long long
-extern unsigned int LDBL_STR_LEN; // The maximum length of a string made from a Long double
+extern unsigned int SHR_STR_LEN;   // The maximum length of a string made from a Short
+extern unsigned int INT_STR_LEN;   // The maximum length of a string made from an Integer
+extern unsigned int FLT_STR_LEN;   // The maximum length of a string made from a Float
+extern unsigned int DBL_STR_LEN;   // The maximum length of a string made from a Double
+extern unsigned int LONG_STR_LEN;  // The maximum length of a string made from a Long
+extern unsigned int LL_STR_LEN;    // The maximum length of a string made from a Long long
+extern unsigned int LDBL_STR_LEN;  // The maximum length of a string made from a Long double
 
 typedef struct Hmap {
     char *key;
-    void *value;
-    enum valuetype valuetype;
+    json_value_t value;
     struct Hmap *next;
 } obj_t; // Object
 
 typedef struct Linkedlist {
-    void *value;
-    enum valuetype valuetype;
+    json_value_t value;
     struct Linkedlist *next;
 } array_t; // Array
+
+typedef struct {
+    enum valuetype vt;
+    union {
+        struct {
+            char *str;
+            size_t len;
+        } str;
+        short s;
+        int i;
+        float f;
+        double dbl;
+        long lg;
+        long long lgl;
+        long double ldbl;
+        obj_t *obj;
+        array_t *arr;
+    } value;
+} json_value_t;
 
 #endif // HASHMAP_STRUCT_H
