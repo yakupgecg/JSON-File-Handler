056584572838e1197d881629124d2ed570f7bef4
Yakup <yakup.jarvenpaa@hotmail.com>
2025-10-17 15:29:37 +0300

    Delete encoder functions from json_format.c because of poor string handling and repetitive strcat and snprintf

--- Headers/json_format.h
+++ Headers/json_format.h
@@ -8,9 +8,6 @@
 /* This header defines the encoder. And if I will ever make the decoder, it will be here.
 I made made functions seperately from functions.h since they serve a purpose for JSON files */
 
-char *encode_pair(obj_t*);
-char *encode_map(obj_t*);
-char *encode_list(array_t*);
 char *indent_json(char *, unsigned int, unsigned int);
 obj_t *decode_pair(char*);
 
