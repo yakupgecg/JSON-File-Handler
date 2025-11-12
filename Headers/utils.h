b9198cca91289e9fbcaf8d0d18ac0aae7a3e8729
Yakup <yakup.jarvenpaa@hotmail.com>
2025-11-12 20:56:30 +0200

    Remve function pr_pairbykey() from utils.h since it is not used in utils.c

--- Headers/utils.h
+++ Headers/utils.h
@@ -9,12 +9,12 @@
 #include <string.h>
 #include <errno.h>
 
-/* This header defines functions for handling hash maps and lists */
+/* This header defines functions for handling objects and lists */
 
 //Returns the valuetype as string representation.
 char *get_vt(enum valuetype);
 
-// Returns the length of the hashmap, but if the pair is not the root of the hashmap, it will start from pair
+// Returns the length of the hashmap, but if the object is not the root of the hashmap, it will start from object
 size_t map_len(obj_t *);
 
 // Returns the length of the list
@@ -23,7 +23,7 @@ size_t list_len(array_t *);
 //Frees a json value
 int free_json_value(json_value_t *);
 
-// Forcefully frees a pair
+// Forcefully frees a object
 int free_pair(obj_t *);
 
 // Forcefully frees an element
@@ -35,19 +35,16 @@ int free_map(obj_t *);
 // Frees the list (or frees every element after the given element)
 int free_list(array_t *);
 
-// Returns the pair that has the given key
+// Returns the object that has the given key
 obj_t *pairbykey(obj_t *, char *);
 
 // Returns the element by index in the given list
 array_t *getelementbyindex(array_t *, size_t);
 
-// Returns the pair before the given pair that has the given key, if found
-obj_t *pr_pairbykey(obj_t *, char *);
-
 // This will initalize a hashmap and then return a pointer to it
 obj_t *initM();
 
-// Returns the last pair in a hash map
+// Returns the last object in a hash map
 obj_t *last_pair(obj_t *);
 
 // Returns the last element in a list
@@ -56,7 +53,7 @@ array_t *last_element(array_t *);
 // Initializes a list
 array_t *initL();
 
-// Adds a pair to the end of the given map and returns it
+// Adds a object to the end of the given map and returns it
 obj_t *appendH(obj_t *);
 
 // Adds an element to the end of the given list and returns it
