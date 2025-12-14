bbec69c88e702ed7562261843ba081894fdccad7
Yakup <yakup.jarvenpaa@hotmail.com>
2025-12-14 21:36:50 +0200

    Add 4 examples

---
+++ Examples/Example1/main.c
@@ -0,0 +1,13 @@
+#include "JSON.h"
+
+int main() {
+	char *json = "{\"foo\": \"bar\"}";
+	printf("Test json          : %s\n", json);
+
+	jfh_obj_t *obj = JFH_parse_obj(json);
+    if (!obj) return 1;
+    
+    printf("Key                : %s\nValue              : %s\n", obj->key, obj->value.value.str.str);
+    JFH_free_map(obj);
+    return 0;
+}
---
+++ Examples/Example2/main.c
@@ -0,0 +1,21 @@
+#include "JSON.h"
+
+int main() {
+    jfh_obj_t *obj = JFH_initM();
+    jfh_array_t *list = JFH_initL();
+    if (!obj || !list) return 1;
+    
+    JFH_setintL(list, -7);
+    JFH_setboolL(JFH_appendL(list), true);
+
+    JFH_setstrH(obj, "foo", "bar");
+    JFH_setarrH(obj, "Example list", list);
+
+    char *json = JFH_indent_json(JFH_encode_obj(obj), 4);
+    if (!json) return 1;
+
+    printf("Json: \n%s\n", json);
+    free(json);
+    JFH_free_map(obj);
+    return 0;
+}
